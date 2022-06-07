6848f18ad75167073e5eb819bdb24fb17157d988
fluidog <fluidog@163.com>
2022-06-07 15:31:54 +0800

    Add docker compose profile, make samba server disabled by default.

--- entry.sh
+++ entry.sh
@@ -13,14 +13,6 @@ cat >~/.ssh/config  <<<"StrictHostKeyChecking no"
 
 # Add some public key from the host which you want login from.
 # Warn : Delet or change the keys below to yourself.
-echo >>~/.ssh/authorized_keys \
-"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTqD2o3cVwPSyF3bdKp8SDcsKkpNIfe64Ojm/vnlGmHnRSvzGlf5Hb8LX\
-LLWhHDEGQEOFPVfCcQWXIs9G2KHeSfttK85JTCQaTRiX+0tLoNK+rX86ZgPsfgUVbofSacy2kjSJgqLG+qKC31I6iBRb1SK/\
-4IxWuWJ6FixPzelyNAvElAz8nbbgWtFXtDpybK0ErZZdyqr2dJ/5C3WN1fCPP3YyuSEfcaexM6Hp7u8MM2doeOHfaw3D6dky\
-sIy+6elUaM8s2wBo1YWfcCNApk1WhOurd4PZSD7yD0LcQSUpbE+gL7FlNnpmE6ec+XuYS1Z3iDohGxYL0UfpvXVHO8vfVBSa\
-PN1eeR3ZeAZd2I8vY7UqVCP2UZAWwlT1NpJjtCUl70wqEr+6/kI6oCaE15Uss8QBxDdkmEUgVfn9EcfhALmrF0Gp0WyH0tGW\
-wHnBONfexAJ6Fgwso1NAk9FPlnGWvYohKhVi0BU1KCCurNoTp8v8FMylfM2nOezpLMESw8b0= liuqi@DESKTOP-8V1AJS6"
-
 
 echo >>~/.ssh/authorized_keys \
 "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6aZYJtPRrWujbE9nrlcJrTuC/9CWL2N9rAAse546DIBL\
